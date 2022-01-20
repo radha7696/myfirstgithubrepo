007394047f335b4c7f93a7a89c565474270bf058
radha7696 <98078364+radha7696@users.noreply.github.com>
2022-01-20 15:40:41 +0530

    Create myfile.java

---
+++ myfile.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
